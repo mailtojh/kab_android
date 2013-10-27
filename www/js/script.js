6f01ab5e5b6592fc1486202f4589131051636636
mailtojh <rubys@intertwingly.net>
2013-10-27 23:08:33 +0900

    device test

--- www/js/script.js
+++ www/js/script.js
@@ -37,15 +37,16 @@ var tTelcomp;
 var tTelcell;
 var tId;
 
-// jqm을 시작합니다. - phonegap load 후에 jqm 시작
-uf_jq_initialize = function() {
-	$(document).bind("mobileinit", function() {
-		alert(103)
-		$.support.cors = true;
-		$.mobile.allowCrossDomainPages = true;
-	});
+$(document).bind("mobileinit", function() {
+	$.support.cors = true;
+	$.mobile.allowCrossDomainPages = true;
 
 	alert('jq_start')
+});
+
+// jqm을 시작합니다. - phonegap load 후에 jqm 시작
+uf_jq_initialize = function() {
+
 	db = window.openDatabase("kab_member", "1.0", "KAB Member DB", 1000000);
 
 	// db 생성
