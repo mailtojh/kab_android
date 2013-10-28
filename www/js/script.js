66629b501abca44d886dbdb64dad215fa4f1470e
mailtojh <rubys@intertwingly.net>
2013-10-28 09:27:57 +0900

    zz

--- www/js/script.js
+++ www/js/script.js
@@ -38,14 +38,16 @@ var tTelcomp;
 var tTelcell;
 var tId;
 
-//var gvUrl = "http://kabmobile.mighty-x.com:8080/Mighty/mobile/";
-var gvUrl = "http://www.kab.co.kr/kab/home/mobile/";
+var gvUrl = "http://kabmobile.mighty-x.com:8080/Mighty/mobile/";
+//var gvUrl = "http://www.kab.co.kr/kab/home/mobile/";
 
 
 // jqm을 시작합니다. - phonegap load 후에 jqm 시작
 uf_jq_initialize = function() {
 	gvHandno = window.localStorage.getItem("handno");
 	gvEmpno =  window.localStorage.getItem("empno");
+
+alert(gvHandno + "/"+ gvEmpno + "/" + gvMac)
 
 	if(gvHandno&&gvEmpno&&gvMac) {
 		uf_showLoading("show");
