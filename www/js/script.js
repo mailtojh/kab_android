fb9b9da84b01bacf024b8ff0814daa7766b30cf2
mailtojh <rubys@intertwingly.net>
2013-11-05 06:43:51 +0900

    test data comma

--- www/js/script.js
+++ www/js/script.js
@@ -43,8 +43,8 @@ var tTelcomp;
 var tTelcell;
 var tId;
 
-//var gvUrl = "http://kabmobile.mighty-x.com:8080/Mighty/mobile/";
-var gvUrl = "http://www.kab.co.kr/kab/home/mobile/";
+var gvUrl = "http://kabmobile.mighty-x.com:8080/Mighty/mobile/";
+//var gvUrl = "http://www.kab.co.kr/kab/home/mobile/";
 
 // jqm을 시작합니다. - phonegap load 후에 jqm 시작
 uf_jq_initialize = function() {
@@ -632,7 +632,7 @@ uf_sendsms = function(aPage) {
 		}
 	});
 
-	location.href = "sms:"+tPhone.join(';');
+	location.href = "sms:"+tPhone.join(',');
 	
 }
 
